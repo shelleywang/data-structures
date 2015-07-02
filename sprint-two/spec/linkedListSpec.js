33cd198a96f0799db965fe5020a819bb2ee63bfe
shelleywang <shelleyw13@gmail.com>
2015-07-01 20:07:05 -0700

    Completed LinkedList + Doubly Linked List

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -52,4 +52,45 @@ describe('linkedList', function() {
   });
 
   // add more tests here to test the functionality of linkedList
+
+  it('should return null if removing more items than added', function(){
+    linkedList.addToTail(4);
+    linkedList.removeHead();
+    expect(linkedList.removeHead()).to.equal(null);
+  });
+
+  it('should be able to handle datatypes other than numbers', function() {
+    linkedList.addToTail('Test');
+    var obj1 = {test:'HR'};
+    linkedList.addToTail(obj1);
+    linkedList.addToTail(true);
+    var array1 = [1,2,3];
+    linkedList.addToTail(array1);
+    expect(linkedList.contains('Test')).to.equal(true);
+    expect(linkedList.contains(obj1)).to.equal(true);
+    expect(linkedList.contains(true)).to.equal(true);
+    expect(linkedList.contains(array1)).to.equal(true);
+  });
+
+  /*
+   *  Testing Doubly Linked LIst
+   */
+  var dList = doublyLinkedList();
+  it('doubly-linked-list should be able to add to head', function(){
+    dList.addToHead(4);
+    expect(dList.head.value).to.equal(4);
+    dList.addToHead(5);
+    expect(dList.head.value).to.equal(5);
+  });
+
+  it('doubly-linked-list should be able to remove from tail and return', function(){
+    dList.addToHead(4);
+    dList.addToHead(5);
+    dList.addToTail(6);
+    expect(dList.removeTail()).to.equal(6);
+    expect(dList.removeTail()).to.equal(4);
+    expect(dList.removeTail()).to.equal(5);
+  });
+
+
 });
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var aNode = Node(value);
+    if (list.tail !== null) {
+      list.tail.next = aNode;
+    }
+    else if(list.head === null){
+      list.head = list.tail = aNode;
+    }
+    list.tail = aNode;
   };
 
   list.removeHead = function(){
+    if(list.head === null){
+      return null;
+    }else{
+      var retNode = list.head;
+      list.head = list.head.next;
+      return retNode.value;
+    }
   };
 
   list.contains = function(target){
+    var aNode = list.head;
+    while(aNode !== null){
+      if(aNode.value === target)
+        return true;
+      aNode = aNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -20,10 +42,68 @@ var Node = function(value){
 
   node.value = value;
   node.next = null;
+  node.previous = null;
 
   return node;
 };
-
+  
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail: O(1)
+ * removeHead: O(1)
+ * contains: O(n)
  */
+
+// EXTRA CREDIT: doubly linked list 
+
+var doublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToHead = function(value) {
+    var newNode = Node(value);
+    newNode.next = list.head;
+    if(list.head !== null)
+      list.head.previous = newNode;
+    if (list.tail === null) {
+      list.tail = newNode;
+    }
+    list.head=newNode;
+  };
+
+  list.removeTail = function() {
+    if (list.tail === null){
+      return null;
+    } else {
+      var retNode = list.tail;
+      list.tail = list.tail.previous;
+      list.tail.next = null;
+    }
+    return retNode.value;
+  };
+
+  list.addToTail = function(value){
+    var aNode = Node(value);
+    aNode.previous = list.tail;
+    if (list.tail !== null) {
+      list.tail.next = aNode;
+    } else if(list.head === null){
+      list.head = list.tail = aNode;
+    }
+    list.tail = aNode;
+  };
+
+  list.removeHead = function(value){
+    if (list.head === null){
+        return null;
+    } else {
+      var retNode = list.head;
+      list.head = list.head.next;
+      list.head.previous = null;
+      return retNode.value;
+    }
+  };
+
+  return list;
+};
