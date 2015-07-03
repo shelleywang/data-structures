dde3fd9c180abb1da1d53a918af811cd4009ea6e
shelleywang <shelleyw13@gmail.com>
2015-07-03 11:19:29 -0700

    Somewhat completed HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,18 +5,37 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  // find index i in this._storage
+  // if it already exists, append value NODE to list 
+  // if not, create new list, append value NODE
+  // use this._storage.set to set index i to new linked list 
+  if(this._storage.get(i) !== null)
+
+  this._storage.set(i, v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  // search list of nodes for node with key, return node value
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  // iterate through list at index i to find key 
+  // remove from linked list 
+  this._storage.set(i,null) ;
 };
 
+var hashNode = function(key, value){
+  var node = {};
 
+  node.key = key;
+  node.value = value;
+  node.next = null;
+
+  return node;
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -52,4 +52,9 @@ var getIndexBelowMaxForKey = function(str, max){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * .get: O(1)
+ * .set: O(1)
+ * .each: O(n)
+ * checkLimit: O(1)
+ * getIndexBelowMaxForKey: O(n)
  */
